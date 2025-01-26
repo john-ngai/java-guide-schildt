cd690ee907245ac2c0e9c9e3cacbb8e243ba29d4
mrjohnming <john.ngai@hotmail.com>
2025-01-26 10:06:21 -0500

    Chapter 2-1 example

---
+++ 02_data-types-&-operators/Sound.java
@@ -0,0 +1,10 @@
+class Sound {
+    public static void main(String[] args) {
+        final int feetPerSecond = 1100;
+
+        double seconds = 7.2;
+        double distance = seconds * feetPerSecond;
+
+        System.out.println("The lightning is " + distance + " feet away.");
+    }
+}
