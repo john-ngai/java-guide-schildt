27d96e7ca1b230d9826f84cf7b4defe0b63617bd
John Ngai <john.ngai@arcticwolf.com>
2025-03-07 09:40:15 -0500

    wip - PrimeNumbers.java

--- 02_data-types-&-operators/PrimeNumbers.java
+++ 02_data-types-&-operators/PrimeNumbers.java
@@ -1,10 +1,23 @@
 /**
- * Write a program that finds all of the prime numbers between 2 and 100.
+ * Write a program that finds all the prime numbers between 2 and 100.
  * A prime number is a positive integer that can only be divisible by itself and 1.
  */
 
 class PrimeNumbers {
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
     public static void main(String[] args) {
-        // TBA
+        println("All the prime numbers between 2 and 100:\n");
+
+        for (int num = 2; num <= 100; num++) {
+//            for (int i = 2; i < num; i++) {
+//                if (num % i == 0) {
+//                    println(String.valueOf(num));
+//                    break;
+//                }
+//            }
+        }
     }
 }
